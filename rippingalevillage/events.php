3783ca4ac01bd0bb098dcf8c031e651a94746c5e
DAndrewA <andrew.martin@bourne-grammar.lincs.sch.uk>
2015-10-14 18:20:32 +0100

    query now works and displays on webpage

--- rippingalevillage/events.php
+++ rippingalevillage/events.php
@@ -141,23 +141,23 @@ function Go(){return}
   if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
         // echos the table into the html, with the event's time,date,description and location in the relevant places
-        echo '<table border="0" cellspacing="0" cellpadding="0">';
-        echo '<tbody><tr height="16">';
-        echo '<td width="3" align="LEFT"> <br></td><td width="204" align="RIGHT"><b><font style="FONT-SIZE:10pt" face="Arial" color="#000000">" . $row["date"] . "</font></b></td>';
-        echo '<td width="7" align="LEFT"> <br></td><td width="68" align="CENTER"><font style="FONT-SIZE:10pt" face="Arial" color="#000000">" . $row["time"] . "</font></td>';
-        echo '<td width="10" align="LEFT"> <br></td><td width="298" align="LEFT"><b><font style="FONT-SIZE:10pt" face="Arial" color="#000000">" . $row["title"] . "</font></b></td>';
-        echo '</tr>';
-        echo '</tbody></table>';
-        echo '<table border="0" cellspacing="0" cellpadding="0">';
-        echo '<tbody><tr height="16">';
-        echo '<td width="294" align="LEFT"> <br></td><td width="298" align="LEFT"><i><font style="FONT-SIZE:10pt" face="Arial" color="#800000">" . $row["location"] . "</font></i></td>';
-        echo '</tr>';
-        echo '</tbody></table>';
-        echo '<table border="0" cellspacing="0" cellpadding="0">';
-        echo '<tbody><tr height="14">';
-        echo '<td width="294" align="LEFT"> <br></td><td width="298" align="LEFT"><font style="FONT-SIZE:8pt" face="Tahoma" color="#ffffff">.</font></td>';
-        echo '</tr>';
-        echo '</tbody></table>';
+        echo "<table border='0' cellspacing='0' cellpadding='0'>";
+        echo "<tbody><tr height='16'>";
+        echo "<td width='3' align='LEFT'> <br></td><td width='204' align='RIGHT'><b><font style='FONT-SIZE:10pt' face='Arial' color='#000000'>" . $row["dateAsText"] . "</font></b></td>";
+        echo "<td width='7' align='LEFT'> <br></td><td width='68' align='CENTER'><font style='FONT-SIZE:10pt' face='Arial' color='#000000'>" . $row["timeAsText"] . "</font></td>";
+        echo "<td width='10' align='LEFT'> <br></td><td width='298' align='LEFT'><b><font style='FONT-SIZE:10pt' face='Arial' color='#000000'>" . $row["title"] . "</font></b></td>";
+        echo "</tr>";
+        echo "</tbody></table>";
+        echo "<table border='0' cellspacing='0' cellpadding='0'>";
+        echo "<tbody><tr height='16'>";
+        echo "<td width='294' align='LEFT'> <br></td><td width='298' align='LEFT'><i><font style='FONT-SIZE:10pt' face='Arial' color='#800000'>" . $row["location"] . "</font></i></td>";
+        echo "</tr>";
+        echo "</tbody></table>";
+        echo "<table border='0' cellspacing='0' cellpadding='0'>";
+        echo "<tbody><tr height='14'>";
+        echo "<td width='294' align='LEFT'> <br></td><td width='298' align='LEFT'><font style='FONT-SIZE:8pt' face='Tahoma' color='#ffffff'>.</font></td>";
+        echo "</tr>";
+        echo "</tbody></table>";
     }
   }else{
     echo "<h3>No events were retrieved from the database</h3>";
